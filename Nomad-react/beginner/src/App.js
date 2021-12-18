2b4ed42ed546b9d165516983f7de4d8e04143c71
imhaeunk <imhaeunk@gmail.com>
2021-12-18 16:51:35 +0900

    2021_12_18

---
+++ Nomad-react/beginner/src/App.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [toDo, setToDo] = useState('')
+  const [toDos, setToDos] = useState([])
+
+  const onChange = e => {
+    setToDo(e.target.value)
+  }
+  const onSubmit = e => {
+    e.preventDefault()
+    if (toDo === '') {
+      return
+    }
+    setToDos(curArray => [toDo, ...curArray])
+    setToDo('')
+  }
+  console.log(toDos)
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your todo"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((todo, i) => (
+          <li key={i}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
