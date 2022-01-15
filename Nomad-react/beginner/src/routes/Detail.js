dea2445c6fe51c691c7caf0724c3d2580c9ca529
imhaeunk <imhaeunk@gmail.com>
2022-01-15 23:13:16 +0900

    react-router useParams

--- Nomad-react/beginner/src/routes/Detail.js
+++ Nomad-react/beginner/src/routes/Detail.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 function Detail() {
-  return <h1>Detail</h1>
+  const { id } = useParams()
+  const [detail, setDetail] = useState({})
+
+  const getMovie = async () => {
+    const {
+      data: { movie }
+    } = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json()
+    setDetail(movie)
+  }
+  useEffect(() => {
+    getMovie()
+  }, [])
+
+  return (
+    <>
+      <h1>Detail</h1>
+      <div>{detail.title}</div>
+    </>
+  )
 }
 
 export default Detail
